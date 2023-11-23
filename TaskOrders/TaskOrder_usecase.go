837e316b15933ad0cbbd888616a75b853b89dec0
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-11-23 21:44:01 +0800

    Update TaskOrder_usecase.go

--- TaskOrders/TaskOrder_usecase.go
+++ TaskOrders/TaskOrder_usecase.go
@@ -1,24 +1,41 @@
-package TaskOrder
+package kitchen
 
 import (
-	"Kitchen/DTO"
+	"Kitchen/domain"
 	"context"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 type TaskOrderUsecase struct {
-	Repo *TaskOrderRepo
+	Repo *TaskOrderRepoImpl
 }
 
-func InitTaskOrderUsecase(repo *TaskOrderRepo) *TaskOrderUsecase {
+func InitTaskOrderUsecase(repo *TaskOrderRepoImpl) *TaskOrderUsecase {
 	return &TaskOrderUsecase{
 		Repo: repo,
 	}
 }
 
-func (uc *TaskOrderUsecase) getTaskOrders(ctx context.Context) ([]DTO.TaskOrder, error) {
-	return uc.Repo.getTaskOrders(ctx)
+func (uc *TaskOrderUsecase) NewOrderGroup(ctx context.Context, tableID int) int {
+	if !uc.Repo.CheckOrderGroup(ctx, tableID) {
+		uc.Repo.NewOrderGroup(ctx, tableID)
+		return 200
+	} else {
+		return 500
+	}
 }
 
-func (uc *TaskOrderUsecase) insertTaskOrder(ctx context.Context, task DTO.TaskOrder) error {
-	return uc.Repo.insertTaskOrder(ctx, task)
+func (uc *TaskOrderUsecase) NewKitchenTask(ctx context.Context, item domain.KitchenTask, id int) {
+	item.UUID, _ = uuid.NewUUID()
+	for i := range item.Orders {
+		item.Orders[i].UUID, _ = uuid.NewUUID()
+		item.Orders[i].CreatedAt = time.Now().Unix()
+	}
+	uc.Repo.NewKitchenTask(ctx, item, id)
+}
+
+func (uc *TaskOrderUsecase) GetKitchenTask(ctx context.Context, tableID int) ([]domain.KitchenTask, error) {
+	return uc.Repo.GetKitchenTask(ctx, tableID)
 }
