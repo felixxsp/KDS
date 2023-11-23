8ff71d2933472f92cfb60ee2c885cdeb66e93ed6
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-11-23 21:42:20 +0800

    Create misc.go

---
+++ Domain/misc.go
@@ -0,0 +1,42 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type SpaceInfo struct {
+	UUID      uuid.UUID `json:"space_uuid" bson:"space_uuid"`
+	Number    int       `json:"space_number" bson:"space_number"`
+	ZoneName  string    `json:"zone_name" bson:"zone_name"`
+	GroupCode string    `json:"group_code" bson:"group_code"`
+}
+
+type DeliveryInfo struct {
+	Number      int       `json:"number" bson:"number"`
+	Partner     string    `json:"partner" bson:"partner"`
+	Driver      string    `json:"driver" bson:"driver"`
+	ScheduledAt time.Time `json:"scheduled_at" bson:"scheduled_at"`
+
+	Customer Customer `json:"customer" bson:"customer"`
+}
+
+type QueueInfo struct {
+	UUID     uuid.UUID `json:"space_uuid" bson:"space_uuid"`
+	Number   int       `json:"number" bson:"number"`
+	Customer Customer  `json:"customer" bson:"customer"`
+
+	ScheduledAt time.Time `json:"scheduled_at" bson:"scheduled_at"`
+}
+
+type Customer struct {
+	Name    string `json:"name" bson:"name"`
+	Phone   Phone  `json:"phone" bson:"phone"`
+	Address string `json:"address" bson:"address"`
+}
+
+type Phone struct {
+	CountryCode string `json:"country_code" bson:"country_code"`
+	Number      string `json:"number" bson:"number"`
+}
