3ba4bcd3b4283943cc227f818e91ea9f8887a181
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-11-24 20:40:32 +0800

    Update TaskOrder_usecase.go

--- TaskOrders/TaskOrder_usecase.go
+++ TaskOrders/TaskOrder_usecase.go
@@ -8,17 +8,15 @@ import (
 	"github.com/google/uuid"
 )
 
-type TaskOrderUsecase struct {
-	Repo *TaskOrderRepoImpl
+type TaskOrderUC_Mongo struct {
+	Repo *TaskOrderRepo_mongo
 }
 
-func InitTaskOrderUsecase(repo *TaskOrderRepoImpl) *TaskOrderUsecase {
-	return &TaskOrderUsecase{
-		Repo: repo,
-	}
+type TaskOrderUC_别的数据库 struct {
+	Repo *TaskOrderRepo_别的数据库
 }
 
-func (uc *TaskOrderUsecase) NewOrderGroup(ctx context.Context, tableID int) int {
+func (uc *TaskOrderUC_别的数据库) NewOrderGroup(ctx context.Context, tableID int) int {
 	if !uc.Repo.CheckOrderGroup(ctx, tableID) {
 		uc.Repo.NewOrderGroup(ctx, tableID)
 		return 200
@@ -27,7 +25,36 @@ func (uc *TaskOrderUsecase) NewOrderGroup(ctx context.Context, tableID int) int 
 	}
 }
 
-func (uc *TaskOrderUsecase) NewKitchenTask(ctx context.Context, item domain.KitchenTask, id int) {
+type TaskOrderUC interface {
+	NewOrderGroup(ctx context.Context, tableID int) int
+	NewKitchenTask(ctx context.Context, item domain.KitchenTask, id int)
+	GetKitchenTask(ctx context.Context, tableID int) ([]domain.KitchenTask, error)
+}
+
+func New_TaskOrderUsecase_mongo(repo *TaskOrderRepo_mongo) *TaskOrderUC_Mongo {
+	return &TaskOrderUC_Mongo{
+		Repo: repo,
+	}
+}
+
+func New_TaskOrderUsecase_别的数据库(repo *TaskOrderRepo_别的数据库) *TaskOrderUC_别的数据库 {
+	//邪教代码
+	return &TaskOrderUC_别的数据库{
+		Repo: repo,
+	}
+	//邪教代码
+}
+
+func (uc *TaskOrderUC_Mongo) NewOrderGroup(ctx context.Context, tableID int) int {
+	if !uc.Repo.CheckOrderGroup(ctx, tableID) {
+		uc.Repo.NewOrderGroup(ctx, tableID)
+		return 200
+	} else {
+		return 500
+	}
+}
+
+func (uc *TaskOrderUC_Mongo) NewKitchenTask(ctx context.Context, item domain.KitchenTask, id int) {
 	item.UUID, _ = uuid.NewUUID()
 	for i := range item.Orders {
 		item.Orders[i].UUID, _ = uuid.NewUUID()
@@ -36,6 +63,6 @@ func (uc *TaskOrderUsecase) NewKitchenTask(ctx context.Context, item domain.Kitc
 	uc.Repo.NewKitchenTask(ctx, item, id)
 }
 
-func (uc *TaskOrderUsecase) GetKitchenTask(ctx context.Context, tableID int) ([]domain.KitchenTask, error) {
+func (uc *TaskOrderUC_Mongo) GetKitchenTask(ctx context.Context, tableID int) ([]domain.KitchenTask, error) {
 	return uc.Repo.GetKitchenTask(ctx, tableID)
 }
