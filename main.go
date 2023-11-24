c4b11a9e587536e8ba9cf46641d57f6ae9488c4f
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-11-24 20:41:08 +0800

    Update main.go

--- main.go
+++ main.go
@@ -17,9 +17,13 @@ func main() {
 	client, _ := mongo.Connect(mainCtx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	database := client.Database("kitchen")
 
-	TaskOrderRepo := kitchen.InitTaskOrderRepo(database)
-	TaskorderUsecase := kitchen.InitTaskOrderUsecase(TaskOrderRepo)
-	TaskOrderHandler := kitchen.InitTaskOrderHandler(router, TaskorderUsecase)
+	TaskOrderRepo := kitchen.New_TaskOrderRepo_Mongo(database)
+	TaskorderUsecase := kitchen.New_TaskOrderUsecase_mongo(TaskOrderRepo)
+
+	HerecticRepo := kitchen.New_TaskOrderRepo_别的数据库(database)
+	HerecticUC := kitchen.New_TaskOrderUsecase_别的数据库(HerecticRepo)
+
+	TaskOrderHandler := kitchen.InitTaskOrderHandler(router, TaskorderUsecase, HerecticUC)
 	TaskOrderHandler.Standby(mainCtx)
 
 	//fmt.Println(TaskOrderHandler.UC.GetKitchenTask(mainCtx, 20))
